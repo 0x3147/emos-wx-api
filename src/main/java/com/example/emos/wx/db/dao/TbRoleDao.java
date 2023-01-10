8b075e73b9dde5d9e39e94edb3ae4f2340c6ebf5
康佳星 <jiaxing.kang@hand-china.com>
2023-01-11 00:44:14 +0800

    1.配置MyBatis，生成相关文件 2.创建自定义异常类 3.创建Web返回信息类

---
+++ src/main/java/com/example/emos/wx/db/dao/TbRoleDao.java
@@ -0,0 +1,16 @@
+package com.example.emos.wx.db.dao;
+
+import com.example.emos.wx.db.pojo.TbRole;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @Entity com.example.emos.wx.db.pojo.TbRole
+ */
+@Mapper
+public interface TbRoleDao {
+
+}
---
+++ src/main/java/com/example/emos/wx/exception/EmosException.java
@@ -0,0 +1,25 @@
+package com.example.emos.wx.exception;
+
+import lombok.Data;
+
+@Data
+public class EmosException extends Throwable {
+    public String msg;
+    public int code = 500;
+
+    public EmosException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public EmosException(String msg, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+    }
+
+    public EmosException(String msg, int code, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+        this.code = code;
+    }
+}
