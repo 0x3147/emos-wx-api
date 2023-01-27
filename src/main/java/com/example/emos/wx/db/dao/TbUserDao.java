4d6dfa0fb9fae0c6181b7a1d8abb0209c867ffb3
康佳星 <jiaxing.kang@hand-china.com>
2023-01-27 12:26:17 +0800

    实现持久层注册超级管理员功能

--- src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
+++ src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
@@ -11,4 +11,26 @@ import java.util.Set;
 @Mapper
 public interface TbUserDao {
 
+    /**
+     * @desc 查询是否存在超级管理员
+     * @Author 康佳星
+     * @Date 2023/1/27
+     */
+    boolean haveRootUser();
+
+    /**
+     * @desc 保存用户记录
+     * @Author 康佳星
+     * @Date 2023/1/27
+     * @param param 用户记录
+     */
+    int insert(HashMap param);
+
+    /**
+     * @desc 根据OpenId查询用户ID
+     * @Author 康佳星
+     * @Date 2023/1/27
+     * @param openId 小程序openId
+     */
+    Integer searchIdByOpenId(String openId);
 }
